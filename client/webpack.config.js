42c3914544335cb91af571d3abe29922e93597ec
Aman Sharma <amansha2047@gmail.com>
2024-12-09 18:58:39 -0500

    Bundle UI using Webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,85 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
+const isDevelopment = process.env.NODE_ENV === "development";
+
+module.exports = {
+  mode: isDevelopment ? "development" : "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: isDevelopment ? "bundle.js" : "bundle.[contenthash].js",
+    publicPath: "/",
+    clean: true,
+  },
+  devtool: isDevelopment ? "inline-source-map" : "source-map",
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    historyApiFallback: true,
+    open: true,
+    hot: true,
+    port: 3000,
+    host: "0.0.0.0",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: isDevelopment ? ["react-refresh/babel"] : [],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  optimization: {
+    minimize: !isDevelopment,
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+    splitChunks: false,
+    runtimeChunk: false,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+      minify: !isDevelopment && {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+      },
+    }),
+    new Dotenv(),
+    !isDevelopment &&
+      new MiniCssExtractPlugin({
+        filename: "bundle.[contenthash].css",
+      }),
+    isDevelopment && new ReactRefreshWebpackPlugin(),
+  ].filter(Boolean),
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  performance: {
+    hints: false,
+  },
+};
